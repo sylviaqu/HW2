01bc8cfb38feb1eca21dfdb31fea3144568b4b56
Gaomeizhu Qu <xogoss@GaomeizhudeMacBook-Pro.local>
2016-10-02 21:03:24 -0400

    send message to another screen

--- sos/app/src/main/java/com/example/xogoss/sos/MainActivity.java
+++ sos/app/src/main/java/com/example/xogoss/sos/MainActivity.java
@@ -3,27 +3,22 @@ package com.example.xogoss.sos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.app.Activity;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /*Button button =(Button)findViewById(R.id.button)
-        button.setOnClickListener(View v)
-        {
-            public void onClick(View v)
-            {
-                EditText pwdEt=(EditText) findViewById(R.id.edit_message);
-                TextView pwdTv=(TextView) findViewById(R.id.showpwd);
-                String pwd=pwdEt.getText().toString();
-                pwdTv.setText(pwd);
-            }
-
-        }*/
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
